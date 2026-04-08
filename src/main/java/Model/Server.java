0a5f260fdad563e6d7c32164b480d8d95fd347c7
meradarius299 <120124905+meradarius299@users.noreply.github.com>
2026-04-09 01:10:37 +0300

    Update Server.java

--- src/main/java/Model/Server.java
+++ src/main/java/Model/Server.java
@@ -6,13 +6,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Server implements Runnable {
     private BlockingQueue<Task> tasks = new LinkedBlockingQueue<>();
-    private int waitingPeriod;
-    private int servicePeriod;
+    private AtomicInteger waitingPeriod = new AtomicInteger(0);
     public void addTask(Task newTask) {
         tasks.add(newTask);
-        waitingPeriod += newTask.getWaitingTime();
+        waitingPeriod.addAndGet(newTask.getServiceTime());
     }
-
     @Override
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
@@ -23,14 +21,13 @@ public class Server implements Runnable {
                     int currentService = currentTask.getServiceTime();
                     if (currentService > 0) {
                         currentTask.setServiceTime(currentService - 1);
-                        waitingPeriod -= currentTask.decrementWaitingTime();
+                        waitingPeriod.decrementAndGet();
                     }
                     if (currentTask.getServiceTime() == 0) tasks.poll();
                 }
             } catch (InterruptedException e) { break; }
         }
     }
-
     public Task[] getTasks() { return tasks.toArray(new Task[0]); }
-    public int getWaitingPeriod() { return waitingPeriod; }
+    public int getWaitingPeriod() { return waitingPeriod.get(); }
 }
